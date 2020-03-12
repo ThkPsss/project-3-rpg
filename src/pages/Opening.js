71c075481e8cab751a65c9b6c63d6a71f8451154
Christan Spence <caspence@student.fullsail.edu>
2020-03-12 13:16:17 -0400

    Working dropdown box

--- src/pages/Opening.js
+++ src/pages/Opening.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react"
 import Hero from "../components/Hero"
-import Footer from "../components/Footer"
+import NavBar from "../components/Navbar"
 
 class Opening extends Component {
     handleFormSubmit = event => {
@@ -11,11 +11,13 @@ class Opening extends Component {
     render() {
         return (
             <div>
+                <NavBar />
                 <Hero backgroundImage="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/piltover/en_US/0d65273e55685b53b82f4f0ead52fc167a27f911/assets/img/share/zaun_1200x630.jpg">
-                    <h1>Steam Punk RPG</h1>
-                    <h2>The Steam Punk Battler RPG!</h2>
+                    <div id="main-text">
+                        <h1>Steam Punk RPG</h1>
+                        <h2>The Steam Punk Battler RPG!</h2>
+                    </div>
                 </Hero>
-                <Footer />
             </div>
         )
     }
